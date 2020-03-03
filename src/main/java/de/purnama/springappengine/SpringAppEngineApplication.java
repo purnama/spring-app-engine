001347e71a490993393f1b777e7a84bdfd2f1498
Arthur Purnama <arthur@purnama.de>
2020-03-03 23:36:52 +0700

    initial commit

---
+++ src/main/java/de/purnama/springappengine/SpringAppEngineApplication.java
@@ -0,0 +1,21 @@
+package de.purnama.springappengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringAppEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAppEngineApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello world!";
+    }
+
+}
